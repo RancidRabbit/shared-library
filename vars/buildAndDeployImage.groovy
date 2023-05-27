8976bd17e9e86f7ac8e2b3b3a55b1d22a313aceb
fright <asumerkin@bk.ru>
2023-05-27 20:31:40 +0300

    ...

--- vars/buildAndDeployImage.groovy
+++ vars/buildAndDeployImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    withCredentials([usernamePassword(credentialsId: 'ff48a99f-bb74-4f30-a5e7-7a98a2cffed3', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'nexus-user', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t 209.38.249.127:8083/simple-java:1.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin 209.38.249.127:8083"
         sh 'docker push 209.38.249.127:8083/simple-java:1.1'
