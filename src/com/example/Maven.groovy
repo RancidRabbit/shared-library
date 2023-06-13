fe13c633d40e54efe693786bde11c23b239267e4
fright <asumerkin@bk.ru>
2023-06-13 20:47:03 +0300

    ...

--- src/com/example/Maven.groovy
+++ src/com/example/Maven.groovy
@@ -10,6 +10,17 @@ class Maven implements Serializable {
         this.script = script
     }
 
+    def mavenIncrementalVersion(){
+        script.sh 'mvn build-helper:parse-version versions:set \
+                 -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+                 versions:commit'
+        def matcher = script.readFile('pom.xml') =~ '<version>(.+)</version>'
+        def version = matcher[1][1]
+        script.println(version)
+        script.env.TAG = "$version-$script.BUILD_NUMBER"
+        script.println("${script.TAG}")
+    }
+
     def mavenBuildApp() {
         script.sh 'mvn -DskipTests install'
     }
